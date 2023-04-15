fed9c9b4dab0783522abf024e387746cc462f336
Anisur2024 <mrsdeveloper2024@gmail.com>
2023-04-15 12:47:13 +0600

    first

---
+++ ema-john-simple/src/Components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import fakeData from '../../fakeData/products.JSON'
+
+const Shop = () => {
+    console.log(fakeData);
+    return (
+        <div>
+            this is shop house
+        </div>
+    );
+};
+
+export default Shop;
