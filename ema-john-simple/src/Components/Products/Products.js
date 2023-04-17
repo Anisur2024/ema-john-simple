e3f17d6060e64559e5c685b485355be30e7f4f8c
Anisur2024 <mrsdeveloper2024@gmail.com>
2023-04-18 00:52:03 +0600

    half complete

---
+++ ema-john-simple/src/Components/Products/Products.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Products.css'
+
+
+const Products = (props) => {
+    // console.log(props)
+    const { img, name, seller, price, stock } = props.products;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className='product-name'>{name}</h4>
+                <br />
+                <p><small>{seller}</small></p>
+                <p>${price}</p>
+                <br />
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button className='main-button' onClick={props.handleAddProduct}>add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
--- ema-john-simple/src/Components/Shop/Shop.js
+++ ema-john-simple/src/Components/Shop/Shop.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import fakeData from '../../fakeData/products.JSON'
+import './Shop.css'
+import Products from '../Products/Products';
 
 const Shop = () => {
-    console.log(fakeData);
+    
+    const [products, setProducts] = useState([]);
+   
+    useEffect(()=>{
+        fetch(fakeData)
+        .then((res)=>res.json())
+        .then((data)=>{
+            const first10 = data.slice(0, 10);
+            setProducts(first10);
+        })
+    })
+    
+    const handleAddProduct = () =>{
+        console.log('Product added')
+    }
+
     return (
-        <div>
-            this is shop house
+        <div className='shop-container'>
+
+            <div className="products-container">
+                {
+                    products.map(pd => <Products  products={pd} handleAddProduct = {handleAddProduct} />)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>this is cart</h3>
+            </div>
         </div>
     );
 };
